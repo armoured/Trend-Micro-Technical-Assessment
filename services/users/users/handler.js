66b8fc698e9534670ec639a709d3cb4281203d1d
Mitchell <mitchell_shelton@y7mail.com>
2020-10-09 15:19:51 +1100

    setup users service lambda and basic iam permissions

--- services/users/users/handler.js
+++ services/users/users/handler.js
@@ -1,18 +1,56 @@
 'use strict';
 
-module.exports.create_user = (event, context, callback) => {
+var AWS = require('aws-sdk')
 
-  const response = {
-    statusCode: 200,
+function handle_response(status_code, message, event) {
+  return {
+    statusCode: status_code,
     body: JSON.stringify({
-      message: 'Create User',
+      message: message,
       input: event,
-    }),
+    })
   };
+}
+
+module.exports.create_user = async (event, context, callback) => {
+
+  // TODO
+  // 1. Get Body from event
+  // 2. validate email
+  // 3. check user doesn't exist already by querying GSI where email is hash key
+  // 4. Generate UUID and create user
+
+
+  var dynamodb = new AWS.DynamoDB({apiVersion: '2012-08-10', region: 'ap-southeast-2'});
+  var params = {};
+
+  try { 
+    var result = await dynamodb.listTables(params, (err, data) => {
+      if (err) return err;
+      else return data;         
+    }).promise();
+  } catch (err) {
+    console.log(err, err.stack)
+    console.log("Internal Server Error");
+
+    const response = handle_response(500, 'Internal Server Error', event)
+    callback(null, response);
+    return;
+  }
+
+
+  if (result) {
+    console.log(result);
+  }
+
+
+  
+  const response = handle_response(200, 'Create User', event);
 
   callback(null, response);
-
+  return;
 };
+
 
 
 module.exports.get_users = (event, context, callback) => {
@@ -27,3 +65,4 @@ module.exports.get_users = (event, context, callback) => {
   
     callback(null, response);
 };
+
